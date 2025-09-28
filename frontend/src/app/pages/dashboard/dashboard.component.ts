8c30be5d25e5680ce3b667d3e93a9c5b18ec04b1
Samuel Gallego <samuelgallego441@gmail.com>
2025-09-28 18:13:27 -0500

    feat(front): create structure for page dashboard

---
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
+})
+export class DashboardComponent implements OnInit {
+  currentTime: string = '';
+
+  ngOnInit() {
+    this.updateTime();
+
+    setInterval(() => this.updateTime(), 60000);
+  }
+
+  updateTime() {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString('es-ES', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+  }
+}
