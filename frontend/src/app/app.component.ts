dc8a8bcb7115d3b3fa3195db163d0c4ea9159d94
Samuel Gallego <samuelgallego441@gmail.com>
2025-09-28 00:21:05 -0500

    refactor(front): app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; // ✅ Agrega esta importación
+import { RouterOutlet } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet] // ✅ Agrega RouterOutlet aquí
+  imports: [RouterOutlet] 
 })
 export class AppComponent {
   year = new Date().getFullYear();
