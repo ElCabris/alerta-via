27ffbac853347da12816cb8fcfee1e241970f058
Samuel Gallego <samuelgallego441@gmail.com>
2025-09-28 17:05:36 -0500

    feat(front): add new routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,5 +8,13 @@ export const routes: Routes = [
   { 
     path: 'registro', 
     loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent)
+  },
+  { 
+    path: 'terminos', 
+    loadComponent: () => import('./pages/terms/terms.component').then(m => m.TermsComponent)
+  },
+  { 
+    path: 'privacidad', 
+    loadComponent: () => import('./pages/privacy/privacy.component').then(m => m.PrivacyComponent)
   }
 ];
