7589557396839bff646a934287451bd8fff3ffd0
samdg441 <159508264+samdg441@users.noreply.github.com>
2025-10-02 16:53:00 -0500

    Avatar page (#7)
    * feat(front): change color bottons
    
    * feat(front): fixing messages
    
    * feat(front): edit scrollbar
    
    * feat(front): estructuro for dashboard
    
    * feat(front): fixing form
    
    * feat(front): edit profile page
    
    * feat(front): edit comentaries
    
    * avatar page edit colors

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -22,6 +22,18 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
   },
   { 
+    path: 'reportar-incidente', 
+    loadComponent: () => import('./pages/report-incident/report-incident.component').then(m => m.ReportIncidentComponent)
+  },
+  { 
+    path: 'estadisticas', 
+    loadComponent: () => import('./pages/statistics/statistics.component').then(m => m.StatisticsComponent)
+  },
+  { 
+    path: 'perfil', 
+    loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent)
+  },
+  { 
     path: '**', 
     redirectTo: '' 
   }
